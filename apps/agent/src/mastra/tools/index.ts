9e47ca808b75ca7cdc3c0cd0909a48b8e1242374
Thanh Pham <zlatan.pham@gmail.com>
2025-03-13 13:03:41 +0700

    feat: send message to discord

--- apps/agent/src/mastra/tools/index.ts
+++ apps/agent/src/mastra/tools/index.ts
@@ -1,2 +1,3 @@
 export * from './github';
 export * from './formatter';
+export * from './discord';
--- apps/agent/src/mastra/workflows/index.ts
+++ apps/agent/src/mastra/workflows/index.ts
@@ -1,1 +1,2 @@
-export * from "./github-notify-open-prs";
+export * from './github-notify-open-prs';
+export * from './github-send-list-PRs-to-discord';
