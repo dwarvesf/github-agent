29a2330ba246cb6c167f5ab51648bff4e210fa57
Thanh Pham <zlatan.pham@gmail.com>
2025-03-19 10:47:23 +0700

    chore: make eslint command work

--- apps/discord-bot/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-export default {
-  root: true,
-  extends: ['@packages/eslint-config/base'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: true,
-  },
-};
