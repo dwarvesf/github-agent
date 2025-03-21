96560eb72c1888e5ef1d6d00902b51ef88e7c629
Thanh Pham <zlatan.pham@gmail.com>
2025-03-21 15:04:20 +0700

    fix: format prs and commit list (#24)

--- apps/agent/src/mastra/agents/github-agent.ts
+++ apps/agent/src/mastra/agents/github-agent.ts
@@ -10,12 +10,12 @@ You are a GitHub repository assistant designed to provide users with information
 ## 1/ You can fetch the list of pull requests using the following steps
 - step 1: Get the date range for the pull requests using the 'get-date-range' tool, if you cannot see the date range, then assume the date range is all time
 - step 2: Fetch the list of PRs using get-pr-list-agent then
-- step 3: Pass the retrieved data to the 'format-json-list-to-markdown-table-agent' to convert the data into a well-structured markdown table for easy readability. Make sure the output only contains the markdown table
+- step 3: Pass the retrieved data to the 'format-pull-requests-to-markdown-list-agent' to convert the data into a well-structured markdown list for easy readability
 
 ## 2/ You can fetch the list of commits using the following steps
 - step 1: Get the date range for the commits using the 'get-date-range' tool
 - step 2: Fetch the list of commits using get-commits-agent then
-- step 3: Pass the retrieved data to the 'format-commits-to-markdown-list-agent' to convert the data into a well-structured markdown list for easy readability. Make sure the output only contains the markdown list
+- step 3: Pass the retrieved data to the 'format-commits-to-markdown-list-agent' to convert the data into a well-structured markdown list for easy readability
 
 ## 3/ You can fetch the list of user activities using the following steps
 - step 1: Get the date range for the user activities using the 'get-date-range' tool
@@ -27,6 +27,7 @@ You are a GitHub repository assistant designed to provide users with information
   tools: {
     formatJSONListToMarkdownTable: tools.formatJSONListToMarkdownTable,
     formatCommitList: tools.formatCommitList,
+    formatPullRequestList: tools.formatPullRequestList,
     getPullRequestTool: tools.getPullRequestTool,
     getDateRangeTool: tools.getDateRangeTool,
     getListCommitsTool: tools.getCommitsTool,
