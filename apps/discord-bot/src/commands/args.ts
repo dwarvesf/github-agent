ca8a65a01a4a18abc7eb25bd6d5a3ec5bd197189
swomfire <vucuongJim@gmail.com>
2025-03-25 15:31:17 +0700

    chore: remove unused commands for discord bot

--- apps/discord-bot/src/commands/args.ts
+++ apps/discord-bot/src/commands/args.ts
@@ -3,27 +3,11 @@ import {
   ApplicationCommandOptionType,
 } from 'discord.js'
 
-import { DevCommandName, HelpOption, InfoOption } from '../enums/index.js'
+import { HelpOption, InfoOption } from '../enums/index.js'
 import { Language } from '../models/enum-helpers/index.js'
 import { Lang } from '../services/index.js'
 
 export class Args {
-  public static readonly DEV_COMMAND: APIApplicationCommandBasicOption = {
-    name: Lang.getRef('arguments.command', Language.Default),
-    name_localizations: Lang.getRefLocalizationMap('arguments.command'),
-    description: Lang.getRef('argDescs.devCommand', Language.Default),
-    description_localizations: Lang.getRefLocalizationMap(
-      'argDescs.devCommand',
-    ),
-    type: ApplicationCommandOptionType.String,
-    choices: [
-      {
-        name: Lang.getRef('devCommandNames.info', Language.Default),
-        name_localizations: Lang.getRefLocalizationMap('devCommandNames.info'),
-        value: DevCommandName.INFO,
-      },
-    ],
-  }
   public static readonly HELP_OPTION: APIApplicationCommandBasicOption = {
     name: Lang.getRef('arguments.option', Language.Default),
     name_localizations: Lang.getRefLocalizationMap('arguments.option'),
