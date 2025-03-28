522377a461841f83b8a1d9663971dfa4aaafe9dc
swomfire <vucuongJim@gmail.com>
2025-03-28 16:45:19 +0700

    chore: test merge conflict

--- apps/agent/src/utils/datetime.ts
+++ apps/agent/src/utils/datetime.ts
@@ -2,7 +2,7 @@ import { format as dateFnsFormat } from 'date-fns'
 
 export const formatDate = (
   date: Date,
-  format?: 'yyyy-MM-dd' | 'MMMM d, yyyy',
+  format?: 'yyyy-MM-ddbla bla bla' | 'MMMM d, yyyy',
 ) => dateFnsFormat(date, format ?? 'yyyy-MM-dd')
 
 /**
